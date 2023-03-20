a47d4bf9161bb18fb12ffc16804deb79f62295ed
bnguyen-nanolos <thienphamuser@gmail.com>
2023-03-20 22:33:52 +0700

    Update PJ

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -10,23 +10,25 @@ class Product extends HTTPBaseService {
     return (await this.instance.get('/products', { params })).data
   }
 
-  async detail(id: number, columns?: string[]): Promise<AdminResponse> {
-    return (await this.instance.get(`/admins/${id}`, { params: { columns } })).data
+  async detail(id: number): Promise<AdminResponse> {
+    return (await this.instance.get(`/products/${id}`)).data
   }
 
-  async store(params: CreationParams): Promise<CreationResponse> {
-    return (await this.instance.post('/admins', params)).data
+  async create(params: CreationParams): Promise<CreationResponse> {
+    return (await this.instance.post('/products', params)).data
   }
 
   async update(id: number, params: UpdateParams): Promise<AdminResponse> {
-    return (await this.instance.put(`/admins/${id}`, params)).data
+    return (await this.instance.put(`/products/${id}`, params)).data
   }
 
   delete(id: number): void {
-    this.instance.delete(`/admins/${id}`)
+    this.instance.delete(`/products/${id}`)
   }
 
   deleteAll(ids: number[]): void {
-    this.instance.delete(`/admins/${ids.join(',')}`)
+    this.instance.delete(`/products/${ids.join(',')}`)
   }
 }
+
+export default Product;
--- src/store/ProductStore.ts
+++ src/store/ProductStore.ts
@@ -1,15 +1,21 @@
-import type { ProductType } from '@/types/product';
+import type { ProductType, QueryProductParams } from '@/types/product';
 import { ref, type Ref } from 'vue';
 import { defineStore } from 'pinia';
+import Product from '@/models/Product';
 
 const useProductStore = defineStore('product', () => {
   // State
-  const ProductList = ref<ProductType[]>([]);
+  const productList : Ref<ProductType[]> = ref([]);
+
+  //  Getters
 
   // Actions
+  async function getList(params?: QueryProductParams){
+    const { data } = await new Product().list(params);
+    productList.value = data
+  }
 
-
-  return { };
+  return {productList, getList};
 });
 
 export default useProductStore;
